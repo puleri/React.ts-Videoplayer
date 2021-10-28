c7ba07f52c0b491c89fca466288aa81df1fdd09f
mp4 <puleri.mp4@gmail.com>
2021-10-28 14:33:13 -0700

    added copy to navbar and landing page

--- src/components/KrakenContain.tsx
+++ src/components/KrakenContain.tsx
@@ -7,12 +7,18 @@ const KrakenContain = () => {
     <div>
       <div className="navbar">
           <img className="img" src={logo} />
-          <h4>Shop</h4>
+          <h5>Womens</h5>
+          <h5>Mens</h5>
+          <h5>New arrival</h5>
+          <h5>SALE</h5>
+          <h5>Shop</h5>
+
       </div>
       <div className="hero-photo">
 
           <img src={hero} className="shot" />
           <h1 id="kraken-h1">KRAKEN</h1>
+          <h5 id="kraken-h5">Click here to join the Kraken community and stay up to date on all team information</h5>
           <button className="new-button">NEW COLLECTION</button>
       </div>
     </div>
